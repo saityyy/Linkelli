85a40c5b6075bf368c7d83ad40fe705244082d55
saityyy <62131533+saityyy@users.noreply.github.com>
2023-09-10 18:51:28 +0900

    fix:keywordで投稿を取得できない問題の修正

--- frontend/src/components/functional/get_keyword_post.js
+++ frontend/src/components/functional/get_keyword_post.js
@@ -1,6 +1,6 @@
 export default async function get_post(keyword, start, num) {
     const url = process.env.REACT_APP_API_SERVER_ORIGIN
-        + `/api/v1/set_post/get_keyword_post/?num=${num}&start=${start}&keyword=${keyword}`
+        + `/api/v1/post/get_keyword_post/?num=${num}&start=${start}&keyword=${keyword}`
     const data = await fetch(
         url,
         { mode: "cors", credentials: "include" })
