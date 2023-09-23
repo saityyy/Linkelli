cb368ddb9180f732f6fbebb192c7ce840e11efbd
Ubuntu <ubuntu@ip-172-31-42-70.ap-northeast-1.compute.internal>
2023-09-23 15:17:35 +0000

    デプロイできた

--- backend/linkelli/wsgi.py
+++ backend/linkelli/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linkelli.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linkelli.settings')
 
 application = get_wsgi_application()
